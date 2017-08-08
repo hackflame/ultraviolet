eff0fcfd659443dce7ca557bc2a93214a1497a79
garnetius <garnetius@users.noreply.github.com>
2017-08-08 21:02:35 +0300

    Add the C++ compatibility test build script

---
+++ app/compat.bat
@@ -0,0 +1,32 @@
+:: =============================================================================
+:: Ultraviolet Windows build scripts.
+::
+:: Copyright Kristian Garnét.
+:: -----------------------------------------------------------------------------
+
+@echo off
+cd /d "%~dp0"
+
+:: Variables
+
+set MARCH=-march=native
+set QUANTUM=%CREATIVE%\QUANTUM
+set ULTRAVIOLET=%CREATIVE%\Ultraviolet
+
+:: Create the build directory
+
+if not exist build (
+  mkdir build
+
+  if %ERRORLEVEL% neq 0 (
+    echo "Build error: couldn't create the build directory."
+    exit /b %ERRORLEVEL%
+  )
+)
+
+:: C++ compatibility test
+
+gcc -mconsole %MARCH% ^
+  -c -I%QUANTUM% -I%ULTRAVIOLET% ^
+  %ULTRAVIOLET%\app\compat.cpp -o build\compat.o 2> build\compat.log
+if %ERRORLEVEL% neq 0 (echo "Build error: compat.cpp" && exit /b %ERRORLEVEL%)
