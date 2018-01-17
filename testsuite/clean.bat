c186a4e90a80b60d8f345c6a0ab71a07cc4a4b9d
garnetius <garnetius@users.noreply.github.com>
2018-01-17 08:42:19 +0300

    Change build system to CMake

--- testsuite/clean.bat
+++ testsuite/clean.bat
@@ -1,7 +1,5 @@
 :: =============================================================================
-:: Ultraviolet Windows build scripts.
-::
-:: Clean up the build and testing environment.
+:: Ultraviolet
 ::
 :: Copyright Kristian Garnét.
 :: -----------------------------------------------------------------------------
@@ -10,36 +8,11 @@
 cd /d "%~dp0"
 
 if exist build (
-  del "build\*.o"
-  del "build\*.log"
-
-  rmdir build
+  rmdir /s /q build
 )
 
-if exist test (
-  del "test\8\*.txt"
-  del "test\8\16\*.txt"
-  rmdir test\8\16
-  del "test\8\32\*.txt"
-  rmdir test\8\32
-  rmdir test\8
-
-  del "test\16\*.txt"
-  del "test\16\8\*.txt"
-  rmdir test\16\8
-  del "test\16\32\*.txt"
-  rmdir test\16\32
-  rmdir test\16
-
-  del "test\32\*.txt"
-  del "test\32\8\*.txt"
-  rmdir test\32\8
-  del "test\32\16\*.txt"
-  rmdir test\32\16
-  rmdir test\32
-
-  rmdir test
+if exist converted (
+  rmdir /s /q converted
 )
 
-rem Uncomment to remove the built executable as well
-rem del "utf.exe"
+del /q utf.exe
--- testsuite/run.bat
+++ testsuite/run.bat
@@ -1,7 +1,5 @@
 :: =============================================================================
-:: Ultraviolet Windows build scripts.
-::
-:: Test the UTF converter program and the library.
+:: Ultraviolet
 ::
 :: Copyright Kristian Garnét.
 :: -----------------------------------------------------------------------------
@@ -11,47 +9,47 @@ cd /d "%~dp0"
 
 :: Create directories
 
-mkdir test
+mkdir converted
 
-mkdir test\8
-mkdir test\8\16
-mkdir test\8\32
+mkdir converted\8
+mkdir converted\8\16
+mkdir converted\8\32
 
-mkdir test\16
-mkdir test\16\8
-mkdir test\16\32
+mkdir converted\16
+mkdir converted\16\8
+mkdir converted\16\32
 
-mkdir test\32
-mkdir test\32\8
-mkdir test\32\16
+mkdir converted\32
+mkdir converted\32\8
+mkdir converted\32\16
 
 :: Convert everything
 
-for %%F in (..\samples\*.txt) do (
+for %%F in (samples\*.txt) do (
   rem Convert to UTF-8
-  utf -out=utf-8 < %%F > test\8\%%~nxF
+  utf.exe -out=utf-8 < %%F > converted\8\%%~nxF
 
   rem Convert from UTF-8 to UTF-16
-  utf -out=utf-16 < test\8\%%~nxF > test\8\16\%%~nxF
+  utf.exe -out=utf-16 < converted\8\%%~nxF > converted\8\16\%%~nxF
 
   rem Convert from UTF-8 to UTF-32
-  utf -out=utf-32 < test\8\%%~nxF > test\8\32\%%~nxF
+  utf.exe -out=utf-32 < converted\8\%%~nxF > converted\8\32\%%~nxF
 
   rem Convert to UTF-16
-  utf -out=utf-16 < %%F > test\16\%%~nxF
+  utf.exe -out=utf-16 < %%F > converted\16\%%~nxF
 
   rem Convert from UTF-16 to UTF-8
-  utf -out=utf-8 < test\16\%%~nxF > test\16\8\%%~nxF
+  utf.exe -out=utf-8 < converted\16\%%~nxF > converted\16\8\%%~nxF
 
   rem Convert from UTF-16 to UTF-32
-  utf -out=utf-32 < test\16\%%~nxF > test\16\32\%%~nxF
+  utf.exe -out=utf-32 < converted\16\%%~nxF > converted\16\32\%%~nxF
 
   rem Convert to UTF-32
-  utf -out=utf-32 < %%F > test\32\%%~nxF
+  utf.exe -out=utf-32 < %%F > converted\32\%%~nxF
 
   rem Convert from UTF-32 to UTF-8
-  utf -out=utf-8 < test\32\%%~nxF > test\32\8\%%~nxF
+  utf.exe -out=utf-8 < converted\32\%%~nxF > converted\32\8\%%~nxF
 
   rem Convert from UTF-32 to UTF-16
-  utf -out=utf-16 < test\32\%%~nxF > test\32\16\%%~nxF
+  utf.exe -out=utf-16 < converted\32\%%~nxF > converted\32\16\%%~nxF
 )
